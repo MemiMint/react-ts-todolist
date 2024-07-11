c709121c2e527eef97c6a3804ca020100e544c41
Memi <107126677+Yeferson-Hidalgo@users.noreply.github.com>
2024-07-10 20:59:53 -0400

    remove unused imports

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
 import { Typography} from "@mui/joy/"
 
 export const Login: FC = (): JSX.Element => {
--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import { Typography } from "@mui/joy";
 
 export const Register: FC = (): JSX.Element => {
